912c225201f0665bdcfb226259133a1db8ba2c62
Chelsea Myers <chel.myers@gmail.com>
2015-11-02 10:35:49 -0500

    update tracking

--- client.js
+++ client.js
@@ -6,35 +6,52 @@ if (window.DeviceMotionEvent) {
   window.addEventListener('devicemotion', deviceMotionHandler, false);
 
 } else {
-  document.getElementById("dmEvent").innerHTML = "Not supported."
+  document.getElementByClass("body")[0].innerHTML = "Device Motion Not Supported."
 }
 
 function deviceMotionHandler(eventData) {
-  console.log("moved");
-  var info, xyz = "[X, Y, Z]";
 
-  // Grab the acceleration from the results
   var acceleration = eventData.acceleration;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccel").innerHTML = info;
-
-  // Grab the acceleration including gravity from the results
-  acceleration = eventData.accelerationIncludingGravity;
-  info = xyz.replace("X", acceleration.x);
-  info = info.replace("Y", acceleration.y);
-  info = info.replace("Z", acceleration.z);
-  document.getElementById("moAccelGrav").innerHTML = info;
-
-  // Grab the rotation rate from the results
+  var accelerationGravity = eventData.accelerationIncludingGravity;
   var rotation = eventData.rotationRate;
-  info = xyz.replace("X", rotation.alpha);
-  info = info.replace("Y", rotation.beta);
-  info = info.replace("Z", rotation.gamma);
-  document.getElementById("moRotation").innerHTML = info;
 
-  // // Grab the refresh interval from the results
-  info = eventData.interval;
-  document.getElementById("moInterval").innerHTML = info;
+  $('.acX').innerHTML = acceleration.x;
+  $('.acY').innerHTML = acceleration.y;
+  $('.acZ').innerHTML = acceleration.z;
+
+  $('.acGX').innerHTML = accelerationGravity.x;
+  $('.acGY').innerHTML = accelerationGravity.y;
+  $('.acGZ').innerHTML = accelerationGravity.z;
+
+  $('.alpha').innerHTML = rotation.alpha;
+  $('.beta').innerHTML = rotation.beta;
+  $('.gamma').innerHTML = rotation.gamma;
+
+
+  // var info, xyz = "[X, Y, Z]";
+  //
+  // // Grab the acceleration from the results
+  // var acceleration = eventData.acceleration;
+  // info = xyz.replace("X", acceleration.x);
+  // info = info.replace("Y", acceleration.y);
+  // info = info.replace("Z", acceleration.z);
+  // document.getElementById("moAccel").innerHTML = info;
+  //
+  // // Grab the acceleration including gravity from the results
+  // acceleration = eventData.accelerationIncludingGravity;
+  // info = xyz.replace("X", acceleration.x);
+  // info = info.replace("Y", acceleration.y);
+  // info = info.replace("Z", acceleration.z);
+  // document.getElementById("moAccelGrav").innerHTML = info;
+  //
+  // // Grab the rotation rate from the results
+  // var rotation = eventData.rotationRate;
+  // info = xyz.replace("X", rotation.alpha);
+  // info = info.replace("Y", rotation.beta);
+  // info = info.replace("Z", rotation.gamma);
+  // document.getElementById("moRotation").innerHTML = info;
+  //
+  // // // Grab the refresh interval from the results
+  // info = eventData.interval;
+  // document.getElementById("moInterval").innerHTML = info;
 }
